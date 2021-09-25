a2159892d428e2e649034286ca28caa0c340283b
Jared Bryan <icjbryan@gmail.com>
2021-09-25 08:48:09 -0400

    PasswordEncoder @Bean

--- src/main/java/com/pickaflick/services/UserService.java
+++ src/main/java/com/pickaflick/services/UserService.java
@@ -1,13 +1,16 @@
 package com.pickaflick.services;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -48,6 +51,11 @@ public class UserService implements UserDetailsService{
 		List<SimpleGrantedAuthority> authList = new ArrayList<>();
 		authList.add(new SimpleGrantedAuthority("ROLE_USER"));
 		return authList;
+	}
+	//added code below.
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
 	
