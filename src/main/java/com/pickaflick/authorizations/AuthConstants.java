fa233a73aa38bab100cdba7ed2e06a1762a11bbe
Jared Bryan <icjbryan@gmail.com>
2021-10-06 17:46:52 -0400

    Updated Notation

--- src/main/java/com/pickaflick/authorizations/AuthConstants.java
+++ src/main/java/com/pickaflick/authorizations/AuthConstants.java
@@ -3,6 +3,6 @@ package com.pickaflick.authorizations;
 public class AuthConstants {
 	  public static final String SECRET = "nYknijkL7uD006Fvbcjlj5Xq08xCA09x";
 	  public static final long EXPIRATION_TIME = 432_000_000; // 5 days
-	  public static final String TOKEN_PREFIX = "Bearer ";
+	  public static final String TOKEN_PREFIX = "Bearer "; //we used Bearer because it was in our project. We could have used a JSON Web Token or a number of other options.
 	  public static final String HEADER_STRING = "Authorization";
 	}
--- src/main/java/com/pickaflick/models/User.java
+++ src/main/java/com/pickaflick/models/User.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.*;
 
+//Serialization in Java allows us to convert an Object to stream that we
+//can send over the network or save it as file or store in DB for later usage.
 @Entity
 public class User implements Serializable {
 
